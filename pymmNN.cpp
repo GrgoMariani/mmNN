6053801efa62436802e1ec6b09008a37f2b70ade
grimar2008 <grimar_2008@live.com>
2018-06-20 10:04:10 +0200

    Python Facade
    Added python facade for easier future python integration

--- pymmNN.cpp
+++ pymmNN.cpp
@@ -21,16 +21,67 @@
 
 namespace py = boost::python;
 
+class NN_FACADE{
+private:
+    mmNN::NeuralNetwork *_nn;
+    mmNN::ErrorFunction *_error;
+    int input_size;
+    int output_size;
+public:
+    NN_FACADE(int in_size, int out_size, int output_activation){
+        this->input_size=in_size;
+        this->output_size=out_size;
+        this->_nn = new mmNN::NeuralNetwork(in_size, out_size, output_activation);
+    }
+    
+    ~NN_FACADE(){
+        delete this->_nn;
+        delete this->_error;
+    }
+    
+    void setErrorFunction(int error_type){
+        this->_error = new mmNN::ErrorFunction(error_type);
+    }
+    
+    void forward(py::list li){
+        std::vector<double> data;
+        if(len(li)!=this->input_size){
+            std::cout<<"WRONG INPUT SIZE"<<std::endl;
+            return;
+        }
+        for(int i=0; i<len(li); i++){
+            data.push_back( py::extract<double>(li[i]) );
+        }
+        this->_nn->forwardNetwork(data);
+    }
+    
+};
 
 BOOST_PYTHON_MODULE(pymmNN)
 {
+    
+    
+    py::class_< NN_FACADE >("NeuralNetwork", py::init<int, int, int>())
+    .def("forward", &NN_FACADE::forward);
+    
     py::class_< mmNN::LearningRate >("LearningRate", py::init<double>())
       .def("get", &mmNN::LearningRate::getCurrentLearningRate)
       .def("set", &mmNN::LearningRate::setCurrentLearningRate)
       .def("multiply", &mmNN::LearningRate::multiplyLearningRate);
     
-    py::class_< mmNN::NeuralNetwork >("NeuralNetwork", py::init<int, int>());
+    // ENUMS
+    py::enum_<mmNN::ActivationFunctionTypes>("ActivationType")
+    .value("LINEAR", mmNN::AF_LINEAR).value("RELU", mmNN::AF_RELU)
+    .value("TANH", mmNN::AF_TANH).value("SOFTSTEP", mmNN::AF_SOFTSTEP)
+    .value("BINARY", mmNN::AF_BINARY).value("ARCTAN", mmNN::AF_ARCTAN)
+    .value("SOFTSIGN", mmNN::AF_SOFTSIGN).value("ISRU", mmNN::AF_ISRU)
+    .value("LEAKYRELU", mmNN::AF_LEAKYRELU);
     
+    py::enum_<mmNN::AccumulationType>("AccumulationType")
+    .value("NORMAL", mmNN::ACCUMULATION_NORMAL).value("POOLING", mmNN::ACCUMULATION_POOLING)
+    .value("ABS_POOLING", mmNN::ACCUMULATION_ABS_POOLING).value("EUCLIDEAN", mmNN::ACCUMULATION_EUCLIDEAN);
     
+    py::enum_<mmNN::ErrorType>("ErrorType")
+    .value("LOSS_SQUARED", mmNN::LOSS_SQUARED);
     
 }
